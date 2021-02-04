ae1a3936c7a1766672b901c23bcf2e7de3388427
Sourav-98 <sourav.raychaudhuri@gmail.com>
2021-02-04 14:57:21 +0530

    Update 2 for heroku

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const host = "https://shufflr-heroku-api.herokuapp.com/";
 const port = process.env.PORT || 8080;
 
-const myurl = host;
+const myurl = "https://shufflr-heroku-api.herokuapp.com/";
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, "/static")));
@@ -28,91 +28,91 @@ app.get('/load_playlist', (req, res)=>{
             "track" : "As Long As You Love Me" ,
             "artist" : "Backstreet Boys" ,
             "albumCover" : myurl+"resources/albumCover/As_Long_As_You_Love_Me.jpg",
-            "file" : myurl+"/resources/songs/As_Long_As_You_Love_Me.mp3"
+            "file" : myurl+"resources/songs/As_Long_As_You_Love_Me.mp3"
         },
         {
             "id" : 3,
             "track" : "Back In Black" ,
             "artist" : "AC/DC" ,
             "albumCover" : myurl+"resources/albumCover/Back_In_Black__AC_DC.jpg" ,
-            "file" : myurl+"/resources/songs/Back_In_Black__AC_DC.mp3"
+            "file" : myurl+"resources/songs/Back_In_Black__AC_DC.mp3"
         },
         {
             "id" : 4,
             "track" : "Bailamos" ,
             "artist" : "Enrique Iglesias" ,
             "albumCover" : myurl+"resources/albumCover/Bailamos.jpg" ,
-            "file" : myurl+"/resources/songs/Bailamos.mp3"
+            "file" : myurl+"resources/songs/Bailamos.mp3"
         },
         {
             "id" : 5,
             "track" : "Faded" ,
             "artist" : "Alan Walker" ,
             "albumCover" : myurl+"resources/albumCover/Faded.jpg" ,
-            "file" : myurl+"/resources/songs/Faded.mp3"
+            "file" : myurl+"resources/songs/Faded.mp3"
         },
         {
             "id" : 6,
             "track" : "Highway To Hell" ,
             "artist" : "AC/DC" ,
             "albumCover" : myurl+"resources/albumCover/Highway_to_Hell__AC_DC.jpg" ,
-            "file" : myurl+"/resources/songs/Highway_to_Hell__AC_DC.mp3"
+            "file" : myurl+"resources/songs/Highway_to_Hell__AC_DC.mp3"
         },
         {
             "id" : 7,
             "track" : "I Want It That Way" ,
             "artist" : "Backstreet Boys" ,
             "albumCover" : myurl+"resources/albumCover/I_Want_It_That_Way.jpg" ,
-            "file" : myurl+"/resources/songs/I_Want_It_That_Way.mp3"
+            "file" : myurl+"resources/songs/I_Want_It_That_Way.mp3"
         },
         {
             "id" : 8,
             "track" : "Ignite" ,
             "artist" : "K-391 ft. Alan Walker" ,
             "albumCover" : myurl+"resources/albumCover/Ignite.jpg" ,
-            "file" : myurl+"/resources/songs/Ignite.mp3"
+            "file" : myurl+"resources/songs/Ignite.mp3"
         },
         {
             "id" : 9,
             "track" : "Irresistible" ,
             "artist" : "Fall Out Boy ft. Demi Lovato" ,
             "albumCover" : myurl+"resources/albumCover/Irresistible.jpg" ,
-            "file" : myurl+"/resources/songs/Irresistible.mp3"
+            "file" : myurl+"resources/songs/Irresistible.mp3"
         },
         {
             "id" : 10,
             "track" : "Just The Way you Are" ,
             "artist" : "Bruno Mars" ,
             "albumCover" : myurl+"resources/albumCover/Just_The_Way_You_Are.jpg" ,
-            "file" : myurl+"/resources/songs/Just_The_Way_You_Are.mp3"
+            "file" : myurl+"resources/songs/Just_The_Way_You_Are.mp3"
         },
         {
             "id" : 11,
             "track" : "Levitate" ,
             "artist" : "Imagine Dragons" ,
             "albumCover" : myurl+"resources/albumCover/Levitate.jpg" ,
-            "file" : myurl+"/resources/songs/Levitate.mp3"
+            "file" : myurl+"resources/songs/Levitate.mp3"
         },
         {
             "id" : 12,
             "track" : "Playing With The Boys" ,
             "artist" : "Kenny Loggins" ,
             "albumCover" : myurl+"resources/albumCover/Playing_with_the_Boys.jpg" ,
-            "file" : myurl+"/resources/songs/Playing_with_the_Boys.mp3"
+            "file" : myurl+"resources/songs/Playing_with_the_Boys.mp3"
         },
         {
             "id" : 13,
             "track" : "Radioactive" ,
             "artist" : "Imagine Dragons" ,
             "albumCover" : myurl+"resources/albumCover/Radioactive.jpg" ,
-            "file" : myurl+"/resources/songs/Radioactive.mp3"
+            "file" : myurl+"resources/songs/Radioactive.mp3"
         },
         {
             "id" : 14,
             "track" : "Trigger" ,
             "artist" : "Major Lazer & Khalid" ,
             "albumCover" : myurl+"resources/albumCover/Trigger.jpg" ,
-            "file" : myurl+"/resources/songs/Trigger.mp3"
+            "file" : myurl+"resources/songs/Trigger.mp3"
         }
 
     ]
